e2806e57cbf546c7782a968b61db9398860ccf20
Osman Baş <83305672+ossmanbas@users.noreply.github.com>
2022-09-28 20:04:34 +0300

    Register ve Login DTOları hazırlandı.

---
+++ Business/Abstract/IAuthService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.Concreate;
+using Core.Utilities.Results.Abstract;
+using Core.Utilities.Security.JWT;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IAuthService
+    {
+        IDataResult<User2> Register(UserForRegisterDto userForRegisterDto, string password);
+        IDataResult<User2> Login(UserForLoginDto userForLoginDto);
+        IResult UserExists(string email);
+        IDataResult<AccessToken> CreateAccessToken(User2 user);
+    }
+}
